7a998a03bef31baeb29416d24c98ef1c4cffcf4d
George <georgenico9@gmail.com>
2024-02-12 22:01:06 +0100

    upload images to firebase storage

--- src/createFirebase.ts
+++ src/createFirebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_KEY,
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
